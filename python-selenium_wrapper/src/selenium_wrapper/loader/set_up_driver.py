706b632db1abd29e34a0ff9d00588cb682bc1629
PhilipEHausner <Philip.E.Hausner@gmail.com>
2021-04-06 15:39:21 +0200

    Move chromedriver files to installation folder

--- python-selenium_wrapper/src/selenium_wrapper/loader/set_up_driver.py
+++ python-selenium_wrapper/src/selenium_wrapper/loader/set_up_driver.py
@@ -22,3 +22,22 @@ def get_firefox_driver(headless: bool = False) -> WebDriverWrapper:
     driver.set_script_timeout(5)
 
     return WebDriverWrapper(driver)
+
+
+def get_chrome_driver(headless: bool = False) -> WebDriverWrapper:
+    options = webdriver.chrome.options.Options()
+    options.headless = headless
+    options.add_argument("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                         "Chrome/89.0.4389.114 Safari/537.36")
+    options.add_argument("--lang=de-De")
+
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    if sys.platform == "win32":
+        path = os.path.join(file_path, "..", "driver", "windows", "chromedriver.exe")
+    else:  # Linux
+        path = os.path.join(file_path, "..", "driver", "linux", "chromedriver")
+
+    driver = webdriver.Chrome(executable_path=path, options=options)
+    driver.set_script_timeout(5)
+
+    return WebDriverWrapper(driver)
