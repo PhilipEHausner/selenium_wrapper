2d656ea070e386f4bb4efa6a7aee1e98b226212e
PhilipEHausner <Philip.E.Hausner@gmail.com>
2021-04-06 15:36:02 +0200

    Add copy script for files

--- copy_files_from_src_to_installer.sh
+++ copy_files_from_src_to_installer.sh
@@ -1,1 +1,3 @@
-cp -r src/* python-selenium_wrapper/src/selenium_wrapper/
+cp -r src/driver python-selenium_wrapper/src/selenium_wrapper/
+cp -r src/loader python-selenium_wrapper/src/selenium_wrapper/
+cp -r src/wrapper python-selenium_wrapper/src/selenium_wrapper/
